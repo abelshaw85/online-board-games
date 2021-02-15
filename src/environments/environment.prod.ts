daeb9a76a852f291ebf88a51cad1a2d763f66cc0
AB <a.belshaw85@gmail.com>
2021-02-15 12:56:47 +0000

    Changed resource url to reflect change in site location

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  piecesJson: "/online-shogi-game/assets/json/pieces.json",
-  newsJson: "/online-shogi-game/assets/json/news.json",
+  piecesJson: "/online-board-games/assets/json/pieces.json",
+  newsJson: "/online-board-games/assets/json/news.json",
   serverUrl: "https://dry-cliffs-45416.herokuapp.com"
 };
 
